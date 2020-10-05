0202115fa0d1e91186245dc807bb9779853a0b0f
LAPTOP-TV1OTG8M\yongw <yongwon0824@naver.com>
2020-10-06 03:44:26 +0900

    :bulb:Add Comment

--- src/main/java/com/playground/springboot/hello/web/HelloController.java
+++ src/main/java/com/playground/springboot/hello/web/HelloController.java
@@ -2,9 +2,9 @@ package com.playground.springboot.hello.web;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController//JSON을 반환하는 컨트롤러로 변환시켜주는 어노테이션
 public class HelloController {
-    @GetMapping("/hello")
+    @GetMapping("/hello")//GET요청을 받을수 있는 API를 만들어주는 어노테이션
     public String hello(){
         return "hello";
     }
