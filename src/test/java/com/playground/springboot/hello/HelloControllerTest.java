2a495f8397b81ae83d0f0d29e14e86a272dec7af
LAPTOP-TV1OTG8M\yongw <yongwon0824@naver.com>
2020-10-06 03:44:26 +0900

    :bulb:Added Comment

--- src/test/java/com/playground/springboot/hello/HelloControllerTest.java
+++ src/test/java/com/playground/springboot/hello/HelloControllerTest.java
@@ -11,17 +11,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@RunWith(SpringRunner.class)//테스트를 진행할때 인자 실행자를 실행시킴 -> JUnit과 실행자 인자 사이에 연결자 역할 
+@WebMvcTest(controllers = HelloController.class)//여러 스프링 테스트 어노테이션중 WEB관련 어노테이션
 public class HelloControllerTest {
-    @Autowired
-    private MockMvc mvc;
+    @Autowired//스프링이 관리하는 빈을 주입받음(?)
+    private MockMvc mvc;//웹 API를 테스트 할때 사용 GET, POST등
 
-    @Test
+    @Test//
     public void hello_returns() throws Exception {
         String hello = "hello";
 
-        mvc.perform(get("/hello"))
+        mvc.perform(get("/hello"))//MovkMvc를 통해 /hello로 get요청을 보냄 체이닝으로 응답 검증
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
     }
